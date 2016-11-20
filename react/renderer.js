b5fad1c218489596d5df18ac32593408271844c7
Kennet Vuong <kennet.vuong@knowit.no>
2016-11-20 06:21:14 +0100

    Working react example

---
+++ react/renderer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return(
+    <h1>Hello from react</h1>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"));
