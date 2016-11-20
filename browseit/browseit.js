cd104a82e5841aef3882345e7b54623e9521fc07
Kennet Vuong <kennet.vuong@knowit.no>
2016-11-20 03:26:20 +0100

    Browseit now works visually, need to add hotkeys

--- browseit/browseit.js
+++ browseit/browseit.js
@@ -13,7 +13,12 @@ let mainWindow;
 
 function createBrowseitWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minWidth:800,
+    minHeight:600
+  });
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
